241b6957e0514ca7ca1e786eb269f53418a5ed19
deatil <2217957370@qq.com>
2026-08-17 20:04:13 +0800

    fix ci

--- src/Converter/MultibyteStringConverter.php
+++ src/Converter/MultibyteStringConverter.php
@@ -72,8 +72,8 @@ final class MultibyteStringConverter implements SignatureConverter
 
         while (
             mb_substr($data, 0, self::BYTE_SIZE, '8bit') === self::ASN1_NEGATIVE_INTEGER &&
-            mb_substr($data, 2, self::BYTE_SIZE, '8bit') <= self::ASN1_BIG_INTEGER_LIMIT)
-        {
+            mb_substr($data, 2, self::BYTE_SIZE, '8bit') <= self::ASN1_BIG_INTEGER_LIMIT
+        ) {
             $data = mb_substr($data, 2, null, '8bit');
         }
 
@@ -122,10 +122,10 @@ final class MultibyteStringConverter implements SignatureConverter
 
     private static function retrievePositiveInteger(string $data): string
     {
-        while 
-            (mb_substr($data, 0, self::BYTE_SIZE, '8bit') === self::ASN1_NEGATIVE_INTEGER &&
-            mb_substr($data, 2, self::BYTE_SIZE, '8bit') > self::ASN1_BIG_INTEGER_LIMIT)
-        {
+        while (
+            mb_substr($data, 0, self::BYTE_SIZE, '8bit') === self::ASN1_NEGATIVE_INTEGER &&
+            mb_substr($data, 2, self::BYTE_SIZE, '8bit') > self::ASN1_BIG_INTEGER_LIMIT
+        ) {
             $data = mb_substr($data, 2, null, '8bit');
         }
 
